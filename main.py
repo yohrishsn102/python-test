37330f9e7fbb49835b1e887cd25317892efd2fc5
Agatha Philipppa <qqfizgpupq908@hotmail.com>
2025-12-16 14:39:46 +0800

    Refactor Calculator class structure
    Refactor Calculator class and remove unnecessary whitespace.

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-class Calculator:
-    def add(self, a, b):
-        return a + b
-    
-    def multiply(self, a, b):
-        return a * b
+# Last updated: 2025-12-16 14:39:41
 
-if __name__ == "__main__":
-    calc = Calculator()
-    print(calc.add(5, 3))
+class Calculator:    def add(self, a, b):        return a + b        def multiply(self, a, b):        return a * bif __name__ == "__main__":    calc = Calculator()    print(calc.add(5, 3))
